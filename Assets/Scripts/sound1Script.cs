56e0729bfed25f327142e9ce61b4bb4cd60321d0
Ablang-E <ablange.eric@gmail.com>
2020-11-28 20:25:57 -0600

    Final Version

---
+++ Assets/Scripts/sound1Script.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sound1Script : MonoBehaviour
+{
+    public AudioSource playSound;
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other) {
+            if (!playSound.isPlaying) {
+            Debug.Log("Triggered");
+            anim.SetTrigger("triggered");
+            //animation.SetBool("triggered", true);
+            playSound.Play();
+            
+        }        
+    }
+
+    void OnTriggerExit(Collider other) {
+        //animation.SetBool("triggered", false);
+    }
+}
---
+++ Assets/playMusicScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playMusicScript : MonoBehaviour
+{
+
+    AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //audio = GetComponent<AudioSource>;
+        //audio.Play(0);
+        //Debug.log("Started");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
